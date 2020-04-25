19ccafaaa6eceae9de856eb5da7cab872d487ee1
Gastón Maximiliano Quiroga <gastonmq@gmail.com>
2020-04-25 15:44:51 -0300

    Add project files.

---
+++ KeyedSemaphoreSlim/KeyedSemaphoreSlim.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KeyedSemaphoreSlim
+{
+    public class KeyedSemaphoreSlim
+    {
+        private static ConcurrentDictionary<string, SemaphoreSlim> cConcurrentEntities = new ConcurrentDictionary<string, SemaphoreSlim>();
+        
+        public static void Enter(string pKey)
+        {
+            SemaphoreSlim mSemaphore;
+
+            if (!cConcurrentEntities.TryGetValue(pKey, out mSemaphore))
+            {
+                mSemaphore = new SemaphoreSlim(1);
+
+                // agregamos el semaforo para la clave
+                cConcurrentEntities.TryAdd(pKey, mSemaphore);
+            }
+
+            mSemaphore.Wait();
+            
+        }
+
+        public static void Leave(string pKey)
+        {
+            SemaphoreSlim mSemaphore;
+
+            cConcurrentEntities.TryGetValue(pKey, out mSemaphore);
+
+            mSemaphore.Release();
+        }
+    }
+}
---
+++ KeyedSemaphoreSlim/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KeyedSemaphoreSlim
+{
+    class Program
+    {
+        public static void DoWork(int pKey)
+        {
+            KeyedSemaphoreSlim.Enter(pKey.ToString());
+
+
+            Console.WriteLine("Task {0} starting. Key; {1}", Thread.CurrentThread.ManagedThreadId, pKey);
+            Thread.Sleep(2000);
+            Console.WriteLine("Task {0} finish. Key; {1}", Thread.CurrentThread.ManagedThreadId, pKey);
+
+            KeyedSemaphoreSlim.Leave(pKey.ToString());
+        }
+        static void Main(string[] args)
+        {
+            Task[] Tasks = new Task[10];
+            for (int i = 0; i < 10; i++)
+            {
+
+                var mKey = (i % 2) == 0 ? 1 : 2;
+
+                Tasks[i] = Task.Run(() => DoWork(mKey));
+            }
+            Task.WaitAll(Tasks);
+            Console.WriteLine("Finished processing. Press a key to end.");
+            Console.ReadKey();
+        }
+    }
+}
